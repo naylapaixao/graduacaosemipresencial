e2cd7064bc91dfb3144dd891c405ca31620319b3
nayla <naylapaixao@gmail.com>
2019-05-23 14:25:18 -0300

    Documentation changes

--- js/funcaoEnviar.js
+++ js/funcaoEnviar.js
@@ -1,3 +1,4 @@
+//Função que envia um alerta quando o úsuario faz cadastro
 function EnviarMensagem() {
 
     if (document.getElementById("inputNome").value !="" && document.getElementById("inputEmail").value !=""){

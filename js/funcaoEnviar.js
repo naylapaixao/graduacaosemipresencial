d40d43d9944ac33d187bb3b0338fc94feebb6916
nayla <naylapaixao@gmail.com>
2019-05-23 12:21:23 -0300

    Landing Page com função Js

---
+++ js/funcaoEnviar.js
@@ -0,0 +1,6 @@
+function EnviarMensagem() {
+
+    if (document.getElementById("inputNome").value !="" && document.getElementById("inputEmail").value !=""){
+       alert('Obrigado em breve aguarde nosso contato!');
+    }
+}
